c457f1c91bed98a015244a5f8709cc797206a1b7
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2025-12-30 08:50:49 -0600

    feat: transition input

--- src/lib/components/Input.tsx
+++ src/lib/components/Input.tsx
@@ -1,18 +1,23 @@
 import type { ComponentProps } from "react";
 import { cn } from "../utils";
 
-type Props = { label?: string } & ComponentProps<"input">;
-export function Input({ className, label, ...props }: Props) {
+type Props = { label?: string; error?: string } & ComponentProps<"input">;
+export function Input({ className, error, label, ...props }: Props) {
   return (
     <label className="flex flex-col text-sm font-medium text-blue-900">
       {label}
       <input
         className={cn(
-          "rounded-sm border border-gray-500 p-2 ring-offset-0 outline-purple-600 user-invalid:border-red-500",
+          "rounded-sm border border-gray-500 p-2 ring-offset-0 outline-purple-600 transition-all duration-300 user-invalid:border-red-500",
           className,
         )}
         {...props}
       />
+      {
+        <span className={cn("text-red-500", { ["invisible"]: !error })}>
+          {error ?? "hidden error"}
+        </span>
+      }
     </label>
   );
 }
